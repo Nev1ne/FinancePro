94d57e9ff41773c9799870b06533ae21d992919d
Nevine Achola <nevine.achola@student.moringaschool.com>
2023-06-03 21:48:14 +0300

    add files

---
+++ assets/js/charts-lines.js
@@ -0,0 +1,67 @@
+/**
+ * For usage, visit Chart.js docs https://www.chartjs.org/docs/latest/
+ */
+const lineConfig = {
+    type: 'line',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          label: 'Organic',
+           /* 
+           * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+           */
+          backgroundColor: '#0694a2',
+          borderColor: '#0694a2',
+          data: [43, 48, 40, 54, 67, 73, 70],
+          fill: false,
+        },
+        {
+          label: 'Paid',
+          fill: false,
+          /**
+           * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+           */
+          backgroundColor: '#7e3af2',
+          borderColor: '#7e3af2',
+          data: [24, 50, 64, 74, 52, 51, 65],
+        },
+      ],
+    },
+    options: {
+      responsive: true,
+
+      legend: {
+        display: false,
+      },
+      tooltips: {
+        mode: 'index',
+        intersect: false,
+      },
+      hover: {
+        mode: 'nearest',
+        intersect: true,
+      },
+      scales: {
+        x: {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Month',
+          },
+        },
+        y: {
+          display: true,
+          scaleLabel: {
+            display: true,
+            labelString: 'Value',
+          },
+        },
+      },
+    },
+  }
+  
+
+  const lineCtx = document.getElementById('line')
+  window.myLine = new Chart(lineCtx, lineConfig)
+  
